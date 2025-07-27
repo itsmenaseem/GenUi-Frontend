a9cb8abcf314d687cb0ba92398a9e32a8bab846e
Naseem <naseemsiddiqui629@gmail.com>
2025-07-28 00:36:11 +0530

    favicon updated

---
+++ src/components/Logo.jsx
@@ -0,0 +1,9 @@
+const Logo = () => (
+  <svg width="100" height="100" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <circle cx="50" cy="50" r="48" stroke="#4F46E5" strokeWidth="4" fill="#EEF2FF"/>
+    <path d="M35 50C35 43.3726 40.3726 38 47 38H60V42H48C44.6863 42 42 44.6863 42 48V52H60V56H42C40.3431 56 39 54.6569 39 53V50Z" fill="#4F46E5"/>
+    <path d="M60 38H65V62H60V38Z" fill="#4F46E5"/>
+  </svg>
+);
+
+export default Logo;
